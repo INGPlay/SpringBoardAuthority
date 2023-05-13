9d252575b15df038022407c035099850078815c2
김병주 <atker14@gmail.com>
2023-05-13 15:20:10 +0900

    init

---
+++ src/test/java/crud/board/BoardAuthority/BoardAuthorityApplicationTests.java
@@ -0,0 +1,13 @@
+package crud.board.BoardAuthority;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardAuthorityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
